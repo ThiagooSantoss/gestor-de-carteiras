3056bf6334032bf9e579b8eca1faf137ed2e5599
infinityfile <diego05.pimentel.com@gmail.com>
2024-01-19 21:01:38 -0300

    Criação de rotas e melhorias na sidebar

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import { Icon } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
 
 interface SidebarItemProps {
   texto: string;
   Icone: Icon;
   isAtivo?: boolean;
+  url: string;
 }
 
 export const SidebarItem = (props: SidebarItemProps) => {
-  const { texto, Icone, isAtivo = false } = props;
+  const { url, texto, Icone, isAtivo = false } = props;
+
+  const { push } = useRouter();
 
   const itemClassesExtras = isAtivo
     ? "bg-blue-100 p-2 rounded text-gray-700 hover:text-gray-700"
-    : "text-white hover:text-blue-300 cursor-pointer";
+    : "text-white hover:text-blue-300";
 
   return (
     <li
-      className={`${itemClassesExtras} w-fit transition-colors flex gap-2 items-center text-xl`}
+      className={`${itemClassesExtras} w-fit transition-colors flex gap-2 items-center text-xl cursor-pointer`}
+      onClick={() => push(url)}
     >
       <Icone size={32} />
       <p>{texto}</p>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { usePathname, useRouter } from "next/navigation";
 import { SidebarItem } from "./SidebarItem";
 import {
   ChartLineUp,
@@ -9,17 +10,38 @@ import {
 } from "@phosphor-icons/react";
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+  const { push } = useRouter();
+
   return (
     <aside className="bg-slate-900 h-screen w-72 p-6">
-      <header className="text-white flex gap-2 justify-center items-center pb-6 mb-6 -mx-6 border-b text-xl">
+      <header
+        onClick={() => push("/")}
+        className="text-white flex gap-2 justify-center items-center pb-6 mb-6 -mx-6 border-b text-xl cursor-pointer"
+      >
         <CurrencyCircleDollar size={32} />
         <p>Gestor de Carteiras</p>
       </header>
 
       <ul className="space-y-6">
-        <SidebarItem texto="Ativos" Icone={ChartLineUp} />
-        <SidebarItem texto="Proventos" Icone={PiggyBank} isAtivo />
-        <SidebarItem texto="Carteiras" Icone={Wallet} />
+        <SidebarItem
+          isAtivo={pathname == "/ativos"}
+          url="/ativos"
+          texto="Ativos"
+          Icone={ChartLineUp}
+        />
+        <SidebarItem
+          isAtivo={pathname == "/proventos"}
+          url="/proventos"
+          texto="Proventos"
+          Icone={PiggyBank}
+        />
+        <SidebarItem
+          isAtivo={pathname == "/carteiras"}
+          url="/carteiras"
+          texto="Carteiras"
+          Icone={Wallet}
+        />
       </ul>
     </aside>
   );
