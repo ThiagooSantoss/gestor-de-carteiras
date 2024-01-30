85202511d2a0f018ea5d570cb266fc235abbf486
Matheus Pimentel <matheus08.pimentel@gmail.com>
2024-01-29 21:00:26 -0300

    Adicionado configuração de expansão de sub menu

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { CaretDown, Icon } from "@phosphor-icons/react";
+import { CaretDown, CaretRight, Icon } from "@phosphor-icons/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface SidebarItemProps {
   texto: string;
@@ -16,26 +17,44 @@ export const SidebarItem = (props: SidebarItemProps) => {
 
   const { push } = useRouter();
 
+  const [estaEscondidoSubMenu, setEstaEscondidoSubMenu] = useState(true);
+
   const itemClassesExtras = isAtivo
     ? "bg-blue-100 p-2 rounded text-gray-700 hover:text-gray-700"
     : "text-white hover:text-blue-300";
 
+  const Seta = estaEscondidoSubMenu ? CaretRight : CaretDown;
+
+  function handleCliqueNoItem() {
+    push(url);
+    setEstaEscondidoSubMenu(!estaEscondidoSubMenu);
+  }
+
   return (
-    <li className={"space-y-2"} onClick={() => push(url)}>
+    <li className={"space-y-2"}>
       <div
         className={`${itemClassesExtras} w-fit transition-colors flex gap-2 items-center text-xl cursor-pointer`}
+        onClick={() => handleCliqueNoItem()}
       >
-        {subMenu.length > 0 && <CaretDown size={16} />}
+        {subMenu.length > 0 && <Seta size={16} />}
+
         <Icone size={32} />
         <p>{texto}</p>
       </div>
-      <ul>
-        {subMenu.map((item) => (
-          <li className="text-white" key={item}>
-            {item}
-          </li>
-        ))}
-      </ul>
+
+      {subMenu.length > 0 && !estaEscondidoSubMenu && (
+        <ul className="ml-6">
+          {subMenu.map((subMenuItem) => (
+            <li
+              onClick={() => push("subItemURL")}
+              key={subMenuItem}
+              className="text-white cursor-pointer w-fit"
+            >
+              {subMenuItem}
+            </li>
+          ))}
+        </ul>
+      )}
     </li>
   );
 };
