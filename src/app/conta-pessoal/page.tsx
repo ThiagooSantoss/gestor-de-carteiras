719a57affa3115cf012f2c2b4f2a6b086e07c9b6
Matheus Pimentel <matheus08.pimentel@gmail.com>
2024-03-06 21:00:20 -0300

    Adicionado mensagem de boas vindas na conta pessoal

--- src/app/conta-pessoal/page.tsx
+++ src/app/conta-pessoal/page.tsx
@@ -3,34 +3,46 @@
 import { CardLogFinanceiro } from "@/components/CardLogFinanceiro";
 import { CardShimmer } from "@/components/CardLogFinanceiro/Card.shimmer";
 import { useUsuario } from "@/hooks/useUsuario";
+import { computaMensagemDeBoasVindas } from "@/utils/computaMensagemDeBoasVindas";
 
 export default function ContaPessoal() {
-  const { data, isFetching: carregando } = useUsuario();
+  const { data: usuario, isFetching: carregando } = useUsuario();
 
-  const { entradas, saidas } = data;
+  const { entradas, saidas } = usuario;
+
+  const mensagemDeBoasVindas = computaMensagemDeBoasVindas(
+    usuario.primeiro_nome
+  );
 
   return (
-    <div className="flex items-center gap-4">
-      {carregando ? (
-        <>
-          <CardShimmer />
-          <CardShimmer />
-        </>
-      ) : (
-        <>
-          <CardLogFinanceiro
-            isEntrada
-            valor={entradas?.total}
-            ultimoRegistro={entradas?.ultimo_registro}
-          />
+    <div>
+      <header className="space-y-4 mb-5">
+        <h1 className="text-4xl font-medium">{mensagemDeBoasVindas}</h1>
+        <h6 className="text-xl">Esta é sua conta pessoal:</h6>
+      </header>
 
-          <CardLogFinanceiro
-            isEntrada={false}
-            valor={saidas?.total}
-            ultimoRegistro={saidas?.ultimo_registro}
-          />
-        </>
-      )}
+      <main className="flex items-center gap-4">
+        {carregando ? (
+          <>
+            <CardShimmer />
+            <CardShimmer />
+          </>
+        ) : (
+          <>
+            <CardLogFinanceiro
+              isEntrada
+              valor={entradas?.total}
+              ultimoRegistro={entradas?.ultimo_registro}
+            />
+
+            <CardLogFinanceiro
+              isEntrada={false}
+              valor={saidas?.total}
+              ultimoRegistro={saidas?.ultimo_registro}
+            />
+          </>
+        )}
+      </main>
     </div>
   );
 }
