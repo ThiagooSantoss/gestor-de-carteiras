e0a2bac2753a3a8a4cf75c9766c06aa2953210d9
Thiago Santos <thiagosantosmunhoz@gmail.com>
2024-03-18 21:28:44 -0300

    Criação do model carteira

--- src/utils/computaDiaEMes.ts
+++ src/utils/computaDiaEMes.ts
@@ -1,7 +1,6 @@
-import { format } from "date-fns";
+import { format, formatISO, parseISO } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-export const computaDiaEMes = (data: string | number | Date) =>
-  format(data ?? 0, "dd 'de' MMMM", {
-    locale: ptBR,
-  });
+export const computaDiaEMes = (data: string) =>
+
+  format(parseISO( data ?? 0), "dd 'de' MMMM");
